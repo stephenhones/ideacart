ce761786788e3aa05526704f925213e4fb7123d8
Stephen Hones <stephen_hones@hotmail.com>
2013-07-26 15:59:26 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideacart::Application.config.secret_token = '3736ca6edd068116c83cb82fe6a399e8175f902c64c74745a0c91c3085b8bf1ce4d85ccc2a3a44ac456762deab36978eb2367306df186489a2dddb33b6f6213a'
